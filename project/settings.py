197c6bf9f0f898514464b116aa20306a26868d1e
Mohamed Gouda <goda61@gmail.com>
2024-04-15 17:01:52 +0200

    Add form, views, template

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Installed Packages :
+    'django_bootstrap5',
+
 
     # My Apps :
     'ml',
--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ml.views import predict
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+    path('', predict),
 ]
