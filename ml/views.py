7f81e5dfb077e864b25a00ee928b3fe498fd7a2f
Mohamed Gouda <goda61@gmail.com>
2024-04-15 18:55:24 +0200

    Add Prediction model

--- ml/views.py
+++ ml/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import pandas as pd
 
 from .models import Iris
 from .forms import IrisForm
@@ -9,5 +10,31 @@ from .forms import IrisForm
 
 def predict(request):
 
-    form = IrisForm()
+    if request.method == 'POST' :
+        form = IrisForm(request.POST)
+
+        if form.is_valid() :
+
+            myform = form.save(commit=False)
+            sepal_length = form.cleaned_data['sepal_length']
+            sepal_width = form.cleaned_data['sepal_width']
+            petal_length = form.cleaned_data['petal_length']
+            petal_width = form.cleaned_data['petal_width']
+
+            #
+            # Prediction :
+            model = pd.read_pickle("model.pickle")
+            resault = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+
+            classification = resault[0]
+
+            # Save :
+            myform.classification = classification
+            myform.save()
+
+            return render(request,'predict.html',{'form':form,'resault':classification})
+            
+    else :
+        form = IrisForm()
+
     return render(request,'predict.html',{'form':form})
